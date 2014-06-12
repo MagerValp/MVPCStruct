9a51b681409b01087cd640b41754dc014c9dbe1d
Per Olofsson <MagerValp@gmail.com>
2014-06-12 08:15:05 +0200

    Initial Commit

---
+++ GUStructPacker/GUStructPacker.h
@@ -0,0 +1,19 @@
+//
+//  GUStructPacker.h
+//  GUStructPacker
+//
+//  Created by Pelle on 2014-06-12.
+//  Copyright (c) 2014 Göteborgs universitet. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for GUStructPacker.
+FOUNDATION_EXPORT double GUStructPackerVersionNumber;
+
+//! Project version string for GUStructPacker.
+FOUNDATION_EXPORT const unsigned char GUStructPackerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GUStructPacker/PublicHeader.h>
+
+
